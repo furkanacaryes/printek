9935072ebf2d3b289c5101b894c3ce28b568b87e
Furkan Acar <furkan@kimbukedi.com>
2018-12-01 07:31:57 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'printek-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'printek';
+}
