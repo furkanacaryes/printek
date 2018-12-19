3adcac0158894c1e208471ea35e3fb6f2a54ed28
Furkan Acar <furkan@kimbukedi.com>
2018-12-20 01:49:11 +0300

    Pre-Offcanvas

--- src/app/end-user/Modules/services/Components/cartridge/cartridge.component.ts
+++ src/app/end-user/Modules/services/Components/cartridge/cartridge.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'printek-cartridge',
   templateUrl: './cartridge.component.html',
-  styleUrls: ['./cartridge.component.scss']
+  styleUrls: [
+    './cartridge.component.scss',
+    './cartridge.responsive.scss'
+  ]
 })
 export class CartridgeComponent implements OnInit {
 
--- src/app/end-user/Modules/services/Components/mobile-device/mobile-device.component.ts
+++ src/app/end-user/Modules/services/Components/mobile-device/mobile-device.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'printek-mobile-device',
   templateUrl: './mobile-device.component.html',
-  styleUrls: ['./mobile-device.component.scss']
+  styleUrls: [
+    './mobile-device.component.scss',
+    './mobile-device.responsive.scss'
+  ]
 })
 export class MobileDeviceComponent implements OnInit {
 
